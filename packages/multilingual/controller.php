fe638aca095e65c4718aa645ffeb4d80b248b8ee
Ryan Tyler <ryan@concrete5.org>
2012-02-20 15:47:36 -0800

    changed version num

--- packages/multilingual/controller.php
+++ packages/multilingual/controller.php
@@ -4,7 +4,7 @@ class MultilingualPackage extends Package {
 
 	protected $pkgHandle = 'multilingual';
 	protected $appVersionRequired = '5.5';
-	protected $pkgVersion = '1.1.2';
+	protected $pkgVersion = '1.1.3dev';
 	
 	public function getPackageDescription() {
 		return t('Translate your site with this free multilingual solution.');
