2f15f5acde89b4bf530cb7008a857dcadf8deade
Andrew Embler <andrew@concrete5.org>
2012-08-17 14:31:44 -0700

    5.6

--- packages/multilingual/tools/switch_language_for_page.php
+++ packages/multilingual/tools/switch_language_for_page.php
@@ -1,5 +1,12 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");?>
-<?php $c = Page::getByID($_REQUEST['cID'], 'RECENT');
+<?php   defined('C5_EXECUTE') or die("Access Denied.");?>
+<?php  
+$class1 = 'icon-select-list';
+$class2 = 'icon-select-page';
+if (version_compare(APP_VERSION, '5.5.2.2', '>')) {
+	$class1 = 'item-select-list';
+	$class2 = 'item-select-page';
+}
+$c = Page::getByID($_REQUEST['cID'], 'RECENT');
 $cp = new Permissions($c);
 $pc = Page::getByPath('/dashboard/multilingual');
 $pcp = new Permissions($pc);
@@ -21,25 +28,28 @@ if ($cp->canRead() && $pcp->canRead()) {
 	// then loop through them and show if a page exists
 	?>
 
-	<ul class="icon-select-list">
-	<?php foreach($ml as $m) {
+	<ul class="<?=$class1?>">
+	<?php   foreach($ml as $m) { 
 		$relatedID = $m->getTranslatedPageID($c); 
 		$icon = $ih->getSectionFlagIcon($m, true);
 		if ($icon) { ?>
-			<li><?php 		} else { ?>
-			<li class="icon-select-page">
-			<?php 		}
+			<li><?php  
+		} else { ?>
+			<li class="<?=$class2?>">
+			<?php  
+		}
 		if ($relatedID && $currentSection->getCollectionID() != $m->getCollectionID()) { 
 				$relatedPage = Page::getByID($relatedID, 'RECENT');
 				
-		?><a <?php if ($icon) { ?>style="background-image: url(<?php echo $icon?>)" <?php } ?> href="<?php echo $nav->getLinkToCollection($relatedPage)?>"><?php echo t('%s: %s', $m->getLanguageText(), $relatedPage->getCollectionName())?></a>
-		<?php } else { ?><span <?php if ($icon) { ?>style="background-image: url(<?php echo $icon?>)" <?php } ?>><?php 			if ($currentSection->getCollectionID() == $m->getCollectionID()) {
+		?><a <?php   if ($icon) { ?>style="background-image: url(<?php  echo $icon?>)" <?php   } ?> href="<?php  echo $nav->getLinkToCollection($relatedPage)?>"><?php  echo t('%s: %s', $m->getLanguageText(), $relatedPage->getCollectionName())?></a>
+		<?php   } else { ?><span <?php   if ($icon) { ?>style="background-image: url(<?php  echo $icon?>)" <?php   } ?>><?php  
+			if ($currentSection->getCollectionID() == $m->getCollectionID()) {
 				print t('Currently Viewing: %s', $c->getCollectionName());
-			} else { ?><?php echo t('%s: None Created', $m->getLanguageText())?></span><?php }
+			} else { ?><?php  echo t('%s: None Created', $m->getLanguageText())?></span><?php   }
 			
 			}?></li>
-	<?php } ?>
+	<?php   } ?>
 	</ul>
 
 
-<?php } ?>
+<?php   } ?>
