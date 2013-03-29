095c85f856f8dd19bf38cb4d3124bf40222b9018
Evan Cooper <evan@concretecms.com>
2013-03-29 10:38:19 -0700

    making switch language block form layout not scroll

--- packages/multilingual/blocks/switch_language/form.php
+++ packages/multilingual/blocks/switch_language/form.php
@@ -1,8 +1,8 @@
-<fieldset class="form-stacked">
-<div class="clearfix">
+<fieldset class="form-stacked" style="padding-top: 20px;">
+<div class=" control-group clearfix">
 	<?php echo $form->label('label', t('Label'))?>
 	<div class="input">
-		<?php echo $form->text('label', $label, array('style' => 'width: 300px'))?>
+		<?php echo $form->text('label', $label)?>
 	</div>
 </div>
 </fieldset>
