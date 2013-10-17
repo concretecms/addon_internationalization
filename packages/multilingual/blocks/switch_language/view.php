36541bc44c2eaa3e31e56e3fe42a096546260eda
Michele Locati <michele@locati.it>
2013-10-17 08:58:46 +0200

    Remove gettext calls to 'Access Denied.' messages
    They're quite useless...

--- packages/multilingual/blocks/switch_language/view.php
+++ packages/multilingual/blocks/switch_language/view.php
@@ -1,4 +1,4 @@
-<?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
 <form method="post" action="<?php echo $action?>">
 	<?php echo $label?>
