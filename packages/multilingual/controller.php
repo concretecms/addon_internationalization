3fe85310ad52661932a3cdb2d0ef5d20355aaf27
hissy <hishikawa@concrete5.co.jp>
2013-11-14 20:44:50 +0900

    Move the default language process to on_before_process event

--- packages/multilingual/controller.php
+++ packages/multilingual/controller.php
@@ -30,10 +30,10 @@ class MultilingualPackage extends Package {
 		}
 		
 		// checks to see if the user should be redirected to the default language home page instead of the / home page.
-		Events::extend('on_start', 'DefaultLanguageHelper', 'checkDefaultLanguage', 'packages/' . $this->pkgHandle . '/helpers/default_language.php');
+		Events::extend('on_before_process', 'DefaultLanguageHelper', 'checkDefaultLanguage', 'packages/' . $this->pkgHandle . '/helpers/default_language.php');
 		
 		// adds the site translation files to the translation library so strings wrapped in t('') will be translated
-		Events::extend('on_start', 'DefaultLanguageHelper', 'setupSiteInterfaceLocalization', 'packages/' . $this->pkgHandle . '/helpers/default_language.php');
+		Events::extend('on_before_process', 'DefaultLanguageHelper', 'setupSiteInterfaceLocalization', 'packages/' . $this->pkgHandle . '/helpers/default_language.php');
 
 		// Ensure's the language tags are set in the header
 		//Events::extend('on_start', 'TranslatedPagesHelper', 'addMetaTags', 'packages/' . $this->pkgHandle . '/helpers/translated_pages.php');
