db657dc20479bc53bafb7f2a0953521c3e9eb7f4
Michele Locati <michele@locati.it>
2014-04-10 10:47:44 +0200

    Load translation files only once
    Newer versions Localization::changeLocale() already load all the
    translation files, so let's avoid loading them twice.

--- packages/multilingual/helpers/default_language.php
+++ packages/multilingual/helpers/default_language.php
@@ -98,32 +98,31 @@ class DefaultLanguageHelper {
 			$locale = DefaultLanguageHelper::getSessionDefaultLocale();
 		}
 		
-		// change core language to translate e.g. core blocks/themes
 		if (strlen($locale)) {
+			// change core language to translate e.g. core blocks/themes. For versions after 5.6.3.1 it'll load also the site and package translations
 			Localization::changeLocale($locale);
-		}
-		
-		// site translations
-		if (is_dir(DIR_LANGUAGES_SITE_INTERFACE)) {
-			if (file_exists(DIR_LANGUAGES_SITE_INTERFACE . '/' . $locale . '.mo')) {
-				$loc = Localization::getInstance();
-				$loc->addSiteInterfaceLanguage($locale);
-			}
-		}
-		
-		// add package translations
-		if(strlen($locale)) {
-			$ms = MultilingualSection::getByLocale($locale);
-			if($ms instanceof MultilingualSection) {
-				$pl = PackageList::get();
-				$installed = $pl->getPackages();
-				foreach($installed as $pkg) {
-					if($pkg instanceof Package) {
-						$pkg->setupPackageLocalization($ms->getLocale());
+
+			if(version_compare(APP_VERSION, '5.6.3.1') <= 0) {
+				// site translations
+				if (is_dir(DIR_LANGUAGES_SITE_INTERFACE)) {
+					if (file_exists(DIR_LANGUAGES_SITE_INTERFACE . '/' . $locale . '.mo')) {
+						$loc = Localization::getInstance();
+						$loc->addSiteInterfaceLanguage($locale);
+					}
+				}
+				
+				// add package translations
+				$ms = MultilingualSection::getByLocale($locale);
+				if($ms instanceof MultilingualSection) {
+					$pl = PackageList::get();
+					$installed = $pl->getPackages();
+					foreach($installed as $pkg) {
+						if($pkg instanceof Package) {
+							$pkg->setupPackageLocalization($ms->getLocale());
+						}
 					}
 				}
 			}
 		}
-		
 	}
 }
